f7bf2b502eb317756b16c3e4cc06d54a1fa57591
ma <ma_yamaki@yahoo.com>
2020-06-08 16:40:36 +0900

    mmmO

--- code/gradient_descent/second.py
+++ code/gradient_descent/second.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from scipy import stats
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression
 
 from sklearn.datasets import load_boston
 dataset = load_boston()
@@ -21,5 +21,23 @@ samples_train, samples_test, label_train, label_test = train_test_split(samples_
 print(samples_train.shape, label_train.shape)
 print(samples_test.shape, label_test.shape)
 
-regressor = LogisticRegression()
+regressor = LinearRegression()
 regressor.fit(samples_train, label_train)
+
+label_pred = regressor.predict(samples_test)
+
+import matplotlib.pyplot as plt
+
+plt.scatter(label_test, label_pred)
+plt.xlabel("Prices")
+plt.ylabel("Predicted Prices")
+plt.title("Prices vs Predicted Prices")
+plt.axis("equal")
+
+from sklearn., metrics import mean_squared_error
+from sklearn.metrics import r2_score
+
+mse = mean_squared_error(label_test, label_pred)
+r2 = r2_score(label_test, label_pred)
+print(mse)
+print(r2)
