a0e2d5ea63d3dad44c0083975ef53be9eb967a4f
ma <ma_yamaki@yahoo.com>
2020-06-08 19:13:52 +0900

    ddddddddddddddddddd

--- code/claster/first.py
+++ code/claster/first.py
@@ -18,3 +18,27 @@ iris_data.feature_names
 
 x = [v[0] for v in iris_data.data]
 y = [v[1] for v in iris_data.data]
+
+trace = go.Scatter(
+    x =x,
+    y =y,
+    mode = 'makers'
+)
+
+layout = go.Layout(title="Iris Dataset",
+                   hovermode='closest',
+                   xaxis= dict(title='sepal length(cm)',
+                               ticklen=5,
+                               zeroline=False,
+                               gridwidth=2,
+                               ),
+                   yaxis=dict(title='sepal width (cm)', ticklen=5,gridwidth=2,),
+                   showlegend=False
+                   )
+data = [trace]
+
+fig= go.Figure(data=data, layout=layout)
+plot(fig)
+
+df = pd.DataFrame(iris_data.data, columns=[iris_data.feature_names])
+df['class'] = [iris_data.target_names[i] for i in iris_data.target]
